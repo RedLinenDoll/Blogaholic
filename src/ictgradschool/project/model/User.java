d29f17986f8361cdffed7871cb0a724256cc1aa2
niuniuanran <irene.anran.niu@gmail.com>
2020-01-25 09:04:27 +1300

    avatar, layout updates

--- src/ictgradschool/project/model/User.java
+++ src/ictgradschool/project/model/User.java
@@ -21,8 +21,9 @@ public class User {
         this.layoutID=layoutID;
     }
 
-    public User(int userID, String avatarPath, String blogName, String blogDescription, String themeColor, int layoutID) {
+    public User(int userID, String username, String avatarPath, String blogName, String blogDescription, String themeColor, int layoutID) {
         this.userID = userID;
+        this.username = username;
         this.avatarPath = avatarPath;
         this.blogName = blogName;
         this.blogDescription = blogDescription;
