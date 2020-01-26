83a1cffb512395b5a8c5c6d9baaddb6968409726
niuniuanran <irene.anran.niu@gmail.com>
2020-01-26 19:52:58 +1300

    authentication demands

--- src/ictgradschool/project/util/AuthenticationUtils.java
+++ src/ictgradschool/project/util/AuthenticationUtils.java
@@ -5,7 +5,7 @@ public class AuthenticationUtils {
     // TODO createUser,  given username and password, create a User object (with authentication info only).
 
 
-    // TODO authenticate, given a User object that contains authentication info
+    // TODO authenticateUser, given a User object that contains authentication info
     //  (created from data queried from database with username) and a user input password, return
     //  a boolean indicating whether username and password match with each other.
 
