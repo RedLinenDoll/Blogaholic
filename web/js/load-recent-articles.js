cbf5b551a6f16a365cf440b19861ae54e1b988a2
apra984 <apra984@aucklanduni.ac.nz>
2020-01-28 13:49:26 +1300

    TODO: welcome page load recent articles

---
+++ web/js/load-recent-articles.js
@@ -0,0 +1,56 @@
+const uriStart = '/blog/';
+// TODO once we decided on our blog website name, the /blog/ part need to be updated.
+
+
+async function loadRecentArticleList() {
+    const postContainer = document.querySelector("#recent-article-list-container");
+    let response = await fetch(`${uriStart}load-recent-articles`);
+    let articleList = await response.json();
+    postContainer.innerHTML="";
+    articleList.forEach(article => {
+            postContainer.appendChild(renderRecentArticleDiv(article));
+        }
+    );
+}
+
+function renderRecentArticleDiv(article) {
+    const articleDiv = document.createElement("div");
+    articleDiv.classList.add("article-div", "page-item-div");
+
+    const fullArticleLink = document.createElement("a");
+    fullArticleLink.classList.add("full-article-link");
+    fullArticleLink.href=`${uriStart}article-view?articleID=${article.articleID}`;
+
+    const articleTitleDiv = document.createElement("div");
+    articleTitleDiv.classList.add("article-title-div");
+    const articleTitle = document.createElement("h2");
+    articleTitleDiv.classList.add("article-title");
+    articleTitle.innerText = article.articleTitle;
+    articleTitleDiv.appendChild(articleTitle);
+
+    const articleBriefDiv = document.createElement("div");
+    articleBriefDiv.classList.add("article-brief-div", "page-item-brief-div");
+    const articleBrief = document.createElement("p");
+    articleBrief.classList.add("article-brief", "page-item-brief");
+    articleBrief.innerText = article.articleBrief;
+    articleBriefDiv.appendChild(articleBrief);
+
+    const articleInfoDiv = document.createElement("div");
+    articleInfoDiv.classList.add("article-info-div", "page-item-info-div");
+    const articleInfo = document.createElement("span");
+    articleInfo.classList.add("article-info", "page-item-info");
+    articleInfo.innerHTML = `Created on ${timestampToLocaleString(article.timeCreated)} · ${article.likesCount} <i class="far fa-thumbs-up like-empty-button like-article" id="like-article-${article.articleID}"></i>· ${article.dislikesCount} <i class="far fa-thumbs-down dislike-empty-button dislike-article-${article.articleID}"></i>`;
+    articleInfoDiv.appendChild(articleInfo);
+
+    fullArticleLink.appendChild(articleTitleDiv);
+    fullArticleLink.appendChild(articleBriefDiv);
+    articleDiv.appendChild(fullArticleLink);
+    articleDiv.appendChild(articleInfoDiv);
+    articleDiv.innerHTML+='<hr class="line-between-articles">';
+    return articleDiv;
+
+}
+
+function timestampToLocaleString(timestamp) {
+    return new Date(timestamp).toLocaleString()
+}
--- web/welcome-view.jsp
+++ web/welcome-view.jsp
@@ -18,16 +18,45 @@
 
 <%--TODO  Once login is achieved, create a welcome page, feeding recent articles. --%>
 <div class="head-container">
-<div class="page-h1-container" id="welcome-message-container">
-    <h1 id="welcome-message">
-Welcome, <img class="inline-avatar" src='<c:url value="/images/avatar/${loggedUser.avatarPath}"/>'>${loggedUser.username}
-    </h1>
-    <button class="link-button">
-        <a href='<c:url value="/blog-view?authorID=${loggedUser.userID}"/>'>
-        Go to my blog &nbsp;&#8594;
-        </a>
-    </button>
+    <div class="page-h1-container" id="welcome-message-container">
+        <h1 id="welcome-message">
+            Welcome, <img class="inline-avatar"
+                          src='<c:url value="/images/avatar/${loggedUser.avatarPath}"/>'>${loggedUser.username}
+        </h1>
+        <button class="link-button">
+            <a href='<c:url value="/blog-view?authorID=${loggedUser.userID}"/>'>
+                Go to my blog &nbsp;&#8594;
+            </a>
+        </button>
+    </div>
 </div>
+
+<div class="body-container" id="recent-article-container">
+    <div id="recent-article-list-container" class="page-item-container">
+        <%--    This part is for demoenstration of jsp structure, and will be cleared once loaded article --%>
+        <div class="article-div page-item-div">
+
+            <a class="full-article-link page-item-link" href='<c:url value="#"/>'>
+                <div class="article-title-div page-item-title-div">
+                    <h2 class="article-title page-item-title">
+                        <%--            article title shows here  --%>
+                    </h2>
+                </div>
+                <div class="article-brief-div page-item-brief-div">
+                    <p class="article-brief page-item-brief">
+                        <%--            article brief shows here --%>
+                    </p>
+                </div>
+            </a>
+            <div class="article-info-div page-item-info-div">
+                <span class="article-info page-item-info">
+                    <%--           creation date , likes and dislikes shows here --%>
+                </span>
+            </div>
+
+        </div>
+    </div>
+
 </div>
 
 </body>
