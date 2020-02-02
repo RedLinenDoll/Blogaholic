a1b76882d021f2375cb5f07c0e16622d6e664640
niuniuanran <irene.anran.niu@gmail.com>
2020-02-02 23:40:31 +1300

    search page to-do

--- src/ictgradschool/project/control/ArticleSearchServlet.java
+++ src/ictgradschool/project/control/ArticleSearchServlet.java
@@ -12,5 +12,8 @@ public class ArticleSearchServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
        String keyword = request.getParameter("search-keyword");
+
+       //TODO write ArticleDAO method to search for article that contains the keyword in the title or content.
+        // TODO find a way to mark the keyword part A different color.
     }
 }
---
+++ web/WEB-INF/view/search-result-view.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: anran
+  Date: 2/02/20
+  Time: 11:36 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Search Result of ${keyword}</title>
+</head>
+<body>
+<%--TODO populate this page with search results--%>
+</body>
+</html>
